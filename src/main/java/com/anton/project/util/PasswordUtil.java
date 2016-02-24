e2496686edaf216c488af36abe1e285bd119a3c7
Antonie <antoxa3030@yandex.ru>
2016-02-24 13:14:51 +0100

    spring security

---
+++ src/main/java/com/anton/project/util/PasswordUtil.java
@@ -0,0 +1,12 @@
+package com.anton.project.util;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Created by Anton on 24.02.16.
+ */
+public class PasswordUtil {
+    public static PasswordEncoder getPasswordEncoder() {
+        return null;
+    }
+}
